bf005066135343f2c13fd74e04e87aff0d5eeb55
Andrew Clugston <andrew.clugston@ucsf.edu>
2025-01-14 11:17:48 -0800

    Updates to reflect_pixel_matrix().

--- R/reflect_pixel_matrix.R
+++ R/reflect_pixel_matrix.R
@@ -3,8 +3,9 @@
 #' @description
 #' Reflect a (non-rotated) pixel matrix across the diagonal (i.e. swap columns
 #' with start/end/seqnames values). Note that the only columns that are swapped
-#' are the seqnames, start, end, start_adj, end_adj, and bin1/bin2 id columns.
-#' Others will need to be swapped manually, for instance via:
+#' are the seqnames, start, end, start_adj, end_adj, and bin1/bin2 columns (If
+#' present in the data, bin_alt1/2 columns will be swapped as well). Others will
+#' need to be swapped manually, for instance via:
 #'
 #' swap_columns(tb_pix,"column1","column2")
 #'
@@ -32,7 +33,10 @@ reflect_pixel_matrix  <- function(tb_pix_in,drop_diagonal=FALSE){
     swap_columns("end1","end2") %>%
     swap_columns("start_adj1","start_adj2") %>%
     swap_columns("end_adj1","end_adj2") %>%
-    swap_columns("bin1_id","bin2_id")
+    swap_columns("bin1_id","bin2_id") %>%
+    # In cases where bin_alt IDs are present (see also: patchwork matrices),
+    # reflect those as well, but don't worry if they're not present.
+    swap_columns(colA="bin_alt1",colB="bin_alt2",ignore_missing = TRUE)
   if(drop_diagonal) tb <- filter(tb,bin1_id != bin2_id)
   return(tb)
 }
